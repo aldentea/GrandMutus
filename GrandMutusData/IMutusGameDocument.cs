cd16b6d78e46f0860da92c4cb46b356b156f22e6
aldentea <aldentea-dev@otegami.aldente.mydns.jp>
2016-02-10 13:32:31 +0900

    OrderCacheを追加しようとしたが...

---
+++ GrandMutusData/IMutusGameDocument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandMutus.Data
+{
+	// (0.8.0.2)
+	public interface IMutusGameDocument
+	{
+		LogsCollection Logs { get; }
+
+		//void AddOrder(QuestionBase question);
+	}
+
+}
