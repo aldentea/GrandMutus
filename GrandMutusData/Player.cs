a0b85069a767ebe94a2c73d738b85074aaa93751
aldentea <aldentea-dev@otegami.aldente.mydns.jp>
2016-11-26 16:32:03 +0900

    PlayersCollectionを簡単に実装。(Bump to 0.8.3.)

---
+++ GrandMutusData/Player.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Linq;
+using System.IO;
+
+using System.Threading.Tasks;
+
+namespace GrandMutus.Data
+{
+	public class Player : INotifyPropertyChanged
+	{
+
+		// インスタンスはMutusDocumentでのみ作成するようにする．
+		// そこでIDを付与する．
+		// 付与するときも，internalなsetterを用いる．
+
+		#region *IDプロパティ
+		public int ID
+		{
+			get { return _id; }
+			internal set { _id = value; }
+		}
+		int _id = -1; // (0.1.2)-1は未設定であることを示す．
+		#endregion
+
+		#region *Nameプロパティ
+		/// <summary>
+		/// プレイヤーの名前を取得／設定します．
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return _name ?? string.Empty;
+			}
+			set
+			{
+				string new_value = string.IsNullOrEmpty(value) ? string.Empty : value;
+				if (Name != new_value)
+				{
+					NotifyPropertyChanging("Name");
+					this._name = new_value;
+					NotifyPropertyChanged("Name");
+				}
+			}
+		}
+		string _name = string.Empty;
+		#endregion
+
+
+
+
+		#region *Maruプロパティ
+		public decimal Maru
+		{
+			get
+			{ return _maru; }
+			set
+			{
+				if (Maru!= value)
+				{
+					NotifyPropertyChanging("Maru");
+					this._maru = value;
+					NotifyPropertyChanged("Maru");
+				}
+			}
+		}
+		decimal _maru = 0;
+		#endregion
+
+		#region *Batsuプロパティ
+		public decimal Batsu
+		{
+			get
+			{ return _batsu; }
+			set
+			{
+				if (Batsu != value)
+				{
+					NotifyPropertyChanging("Batsu");
+					this._batsu = value;
+					NotifyPropertyChanged("Batsu");
+				}
+			}
+		}
+		decimal _batsu = 0;
+		#endregion
+
+		#region *Scoreプロパティ
+		public decimal Score
+		{
+			get
+			{
+				return _score;
+			}
+			set
+			{
+				if (Score != value)
+				{
+					NotifyPropertyChanging("Score");
+					this._score = value;
+					NotifyPropertyChanged("Score");
+				}
+			}
+		}
+		decimal _score = 0;
+		#endregion
+
+
+
+		#region *Parentプロパティ
+		PlayersCollection Parent
+		{
+			get { return _parent; }
+			set
+			{
+				if (this.Parent != value)
+				{
+					this._parent = value;
+					NotifyPropertyChanged("Parent");
+				}
+			}
+		}
+		PlayersCollection _parent = null;
+		#endregion
+
+		/// <summary>
+		/// 自身がparentに追加された時に呼び出します．
+		/// 除外されたときは引数をnullにして呼び出せばいいのかな？
+		/// </summary>
+		/// <param name="parent"></param>
+		public virtual void OnAddedTo(PlayersCollection parent)
+		{
+			this.Parent = parent;
+		}
+
+		// で，PlayersCollectionのCollectionChangedから呼び出す，と．
+
+
+
+
+		#region XML入出力関連
+
+		// どこに置くかは未定．
+		// ここに置くか，XML生成専用のクラスを作成するか...
+
+		// <player id="2" maru="3" batsu="4" score="-5">
+		//   <name>あるでん茶</name>
+		// </player>
+
+
+		// このあたり本来はXNameなんだけど，手抜きをする．
+		public const string ELEMENT_NAME = "player";  // (0.1.2)publicに変更．
+		const string ID_ATTRIBUTE = "id";
+		const string NAME_ELEMENT = "name";
+		const string MARU_ATTRIBUTE = "maru";
+		const string BATSU_ATTRIBUTE = "batsu";
+		const string SCORE_ATTRIBUTE = "score";
+
+		#region *XML要素を生成(GenerateElement)
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="songs_root">曲ファイルを格納しているディレクトリのフルパスです．</param>
+		/// <returns></returns>
+		public XElement GenerateElement()
+		{
+			var element = new XElement(ELEMENT_NAME,
+				new XAttribute(ID_ATTRIBUTE, this.ID),
+				new XElement(NAME_ELEMENT, this.Name),
+				new XAttribute(MARU_ATTRIBUTE, this.Maru),
+				new XAttribute(BATSU_ATTRIBUTE, this.Batsu),
+				new XAttribute(SCORE_ATTRIBUTE, this.Score)
+			);
+			return element;
+		}
+		#endregion
+
+		#region *[static]XML要素からオブジェクトを生成(Generate)
+		public static Player Generate(XElement playerElement)
+		{
+			var player = new Player();
+
+			// XMLからインスタンスを生成するならばIDは常にあるのでは？
+			// →songをインポートする時とかはそうではないかもしれない？ので一応有無をチェックする．
+			// →でも，インポートする時はXML経由ではなくオブジェクト経由で行った方がいいのでは？(ファイル名のパスの扱いとか...)
+			var id_attribute = playerElement.Attribute(ID_ATTRIBUTE);
+			if (id_attribute != null)
+			{
+				player.ID = (int)id_attribute;
+			}
+			player.Name = (string)playerElement.Element(NAME_ELEMENT);
+			var maru = (decimal?)playerElement.Attribute(MARU_ATTRIBUTE);
+			if (maru.HasValue)
+			{ player.Maru = maru.Value; }
+			var batsu = (decimal?)playerElement.Attribute(BATSU_ATTRIBUTE);
+			if (batsu.HasValue)
+			{ player.Batsu = batsu.Value; }
+			var score = (decimal?)playerElement.Attribute(SCORE_ATTRIBUTE);
+			if (score.HasValue)
+			{ player.Score = score.Value; }
+
+			return player;
+		}
+		#endregion
+
+
+		#endregion
+
+
+
+
+		#region INotifyPropertyChanged実装
+
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		protected void NotifyPropertyChanged(string propertyName)
+		{
+			this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+
+		#region INotifyPropertyChanging実装
+		public event PropertyChangingEventHandler PropertyChanging = delegate { };
+		protected void NotifyPropertyChanging(string propertyName)
+		{
+			this.PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+		}
+		#endregion
+
+	}
+}
---
+++ GrandMutusData/PlayersCollection.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Xml.Linq;
+
+using Aldentea.Wpf.Document;
+
+namespace GrandMutus.Data
+{
+	public class PlayersCollection : ObservableCollection<Player>
+	{
+
+		public PlayersCollection()
+		{
+			this.CollectionChanged += PlayersCollection_CollectionChanged;
+		}
+
+
+
+		#region コレクション変更関連
+
+
+
+		void PlayersCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			switch (e.Action)
+			{
+				case NotifyCollectionChangedAction.Add:
+					foreach (var item in e.NewItems)
+					{
+						var player = (Player)item;
+
+						// IDを付与する．
+						// (0.1.2)IDが既に設定されているかどうかを確認．
+						if (player.ID <= 0) // 無効な値．
+						{
+							player.ID = GenerateNewID();
+						}
+						// ☆songのプロパティ変更をここで受け取る？MutusDocumentで行えばここでは不要？
+						// ↑とりあえずこのクラスで使っています。
+						player.PropertyChanging += Player_PropertyChanging;
+						player.PropertyChanged += Player_PropertyChanged;
+						player.OnAddedTo(this);
+					}
+					break;
+				case NotifyCollectionChangedAction.Remove:
+					// →UIから複数曲をまとめて削除すると、既定では1曲ずつこのイベントが呼び出される．それらをまとめてキャッシュする手段がない！？
+					// →仕方がないので，UIの処理を修正し(0.3.4.1)，複数曲の削除をまとめて受け取るようにした．
+
+					//★IList<string> song_files = new List<string>();
+					IList<Player> players = new List<Player>();
+					foreach (var player in e.OldItems.Cast<Player>())
+					{
+						// 削除にあたって、変更通知機能を抑止。
+						player.PropertyChanging -= Player_PropertyChanging;
+						player.PropertyChanged -= Player_PropertyChanged;
+						players.Add(player);
+					}
+
+					// (0.4.4.1)↓UIから曲を削除する場合もMutusDocumentを経由する方針になったので，この処理を削除．
+					// (MutusDocumentを経由せずに)UIから削除される場合もあるので，
+					// ここでOperationCacheの処理を行うことにした．
+					//if (songs.Count > 0)
+					//{
+					//	this.SongsRemoved(this, new ItemEventArgs<IEnumerable<Song>> { Item = songs });
+					//}
+					break;
+				case NotifyCollectionChangedAction.Reset:
+					// ClearしたときにはResetが発生するのか？←そのようです．
+					break;
+
+			}
+		}
+
+		#endregion
+
+		#region アイテム変更関連
+
+		/// <summary>
+		/// 格納されているアイテムのプロパティ値が変化したときに発生します．
+		/// </summary>
+		public event EventHandler<ItemEventArgs<IOperationCache>> ItemChanged = delegate { };
+
+
+		string _nameCache = string.Empty; 
+		decimal _maruCache = 0;
+		decimal _batsuCache = 0;
+		decimal _scoreCache = 0;
+
+		void Player_PropertyChanging(object sender, System.ComponentModel.PropertyChangingEventArgs e)
+		{
+			var player = (Player)sender;
+			switch (e.PropertyName)
+			{
+				case "Name":
+					this._nameCache = player.Name;
+					break;
+				case "Maru":
+					this._maruCache = player.Maru;
+					break;
+				case "Batsu":
+					this._batsuCache = player.Batsu;
+					break;
+				case "Score":
+					this._scoreCache = player.Score;
+					break;
+			}
+		}
+
+		void Player_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			var player = (Player)sender;
+
+			switch (e.PropertyName)
+			{
+				case "Name":
+					this.ItemChanged(this, new ItemEventArgs<IOperationCache>
+					{
+						Item = new PlayerNameChangedCache(player, _nameCache, player.Name)
+					});
+					_nameCache = string.Empty;
+					break;
+				case "Maru":
+					this.ItemChanged(this, new ItemEventArgs<IOperationCache>
+					{
+						Item = new PlayerMaruChangedCache(player, _maruCache, player.Maru)
+					});
+					_maruCache = 0;
+					break;
+				case "Batsu":
+					this.ItemChanged(this, new ItemEventArgs<IOperationCache>
+					{
+						Item = new PlayerBatsuChangedCache(player, _batsuCache, player.Batsu)
+					});
+					_batsuCache = 0;
+					break;
+				case "Score":
+					this.ItemChanged(this, new ItemEventArgs<IOperationCache>
+					{
+						Item = new PlayerScoreChangedCache(player, _scoreCache, player.Score)
+					});
+					_scoreCache = 0;
+					break;
+			}
+			// ドキュメントにNotifyしたい！？
+			//e.PropertyName
+		}
+
+		#endregion
+
+
+		#region XML入出力関連
+
+		public const string ELEMENT_NAME = "players";
+		const string PATH_ATTRIBUTE = "path";
+
+		// <players>
+		//   <player id="2" maru="3" batsu="4" score="-5">
+		//     <name>あるでん茶</name>
+		//   </player>
+		//   <player id="4" maru="1" batsu="0" score="2">
+		//     <name>たぬき</name>
+		//   </player>
+		// </players>
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="destinationDir">XMLファイルを出力するディレクトリです．</param>
+		/// <returns></returns>
+		public XElement GenerateElement()
+		{
+			XElement element = new XElement(ELEMENT_NAME);
+
+			foreach (Player player in this.Items)
+			{
+				element.Add(player.GenerateElement());
+			}
+
+			return element;
+		}
+
+		public void LoadElement(XElement playersElement)
+		{
+			foreach (var player_element in playersElement.Elements(Player.ELEMENT_NAME))
+			{
+				this.Add(Player.Generate(player_element));
+			}
+		}
+
+		#endregion
+
+
+		#region ID管理関連
+
+		// (0.2.1)無効なIDとして負の値を使うことにしたので，Anyに条件を付加．
+		int GenerateNewID()
+		{
+			int new_id = this.UsedIDList.Any(n => n > 0) ? this.UsedIDList.Max() + 1 : 1;
+			// ↑Max()は，要素が空ならInvalidOperationExceptionをスローする．
+
+			//UsedIDList.Add(new_id);
+			return new_id;
+		}
+
+		IEnumerable<int> UsedIDList
+		{
+			get
+			{
+				return Items.Select(player => player.ID);
+			}
+		}
+
+		#endregion
+
+	}
+}
